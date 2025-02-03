391a76a2a536e0db4953608e1a74a8ae02ec76b6
Tahasiraj1 <tahasiraj242@gmail.com>
2025-02-04 00:21:24 +0500

    Remove unused Image import in CustomersPage for cleaner code

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import { BarLoader } from "react-spinners";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 
